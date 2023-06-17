41fcbd4f1899d7302aa12e691df369715571bf2a
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-06-17 02:11:25 -0400

    create navbar and footer

---
+++ loaders.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var navbarPlaceholder = document.getElementById('navbar-placeholder');
+
+  fetch('components/navbar.html')
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(html) {
+      navbarPlaceholder.innerHTML = html;
+
+      // Update the href values based on the current page
+      var logoLink = navbarPlaceholder.querySelector('.logo');
+      var angryLink = navbarPlaceholder.querySelector('.angry-btn');
+      var formLink = navbarPlaceholder.querySelector('.form-btn');
+      var hoverLink = navbarPlaceholder.querySelector('.hover-btn');
+
+      logoLink.href = 'index.html';
+      angryLink.href = 'buttons.html';
+      formLink.href = 'form.html';
+      hoverLink.href = 'hovering.html';
+    })
+    .catch(function(error) {
+      console.log('Error loading navbar component:', error);
+    });
+    
+});
